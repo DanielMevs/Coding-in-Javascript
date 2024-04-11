4fed362df218d3ac6613e0c375bad3bdf70cc7dc
DanielMevs <dannymevs@hotmail.com>
2024-04-10 20:55:35 -0400

    completed variables and values section

---
+++ fundamentals_part_I/coding_assignments/values_and_variables/solution.js
@@ -0,0 +1,7 @@
+let country = "USA";
+let continent = "North America";
+let population = 300_000_000;
+
+console.log(country);
+console.log(continent);
+console.log(population);
--- fundamentals_part_I/hello_world/hello_world.js
+++ fundamentals_part_I/hello_world/hello_world.js
@@ -1,6 +1,11 @@
-alert("Hello World!");
+// alert("Hello World!");
 let js = "amazing";
-if (js === "amazing") alert("Javascript is FUN!");
-js = "boring";
-if (js === "amazing") alert("Javascript is FUN!");
+// if (js === "amazing") alert("Javascript is FUN!");
+// js = "boring";
+// if (js === "amazing") alert("Javascript is FUN!");
 console.log(40 + 8 + 23 - 10);
+console.log("Daniel");
+console.log(23);
+
+let firstName = "Daniel";
+console.log(firstName);
