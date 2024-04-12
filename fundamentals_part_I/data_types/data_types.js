4f7c981eeb891a071d9e4ce23fd0c678f4317ac4
DanielMevs <dannymevs@hotmail.com>
2024-04-11 21:20:59 -0400

    completed data types section

---
+++ fundamentals_part_I/data_types/data_types.js
@@ -0,0 +1,18 @@
+let javascriptIsFun = true;
+console.log(javascriptIsFun);
+// console.log(typeof true);
+console.log(typeof javascriptIsFun);
+// console.log(typeof 23);
+// console.log(typeof "Daniel");
+
+javascriptIsFun = "Yes";
+console.log(typeof javascriptIsFun);
+
+let year;
+console.log(year);
+console.log(typeof year);
+
+year = 1991;
+console.log(typeof year);
+
+console.log(typeof null);
--- fundamentals_part_I/hello_world/hello_world.js
+++ fundamentals_part_I/hello_world/hello_world.js
@@ -1,11 +1,27 @@
+/*
+
 // alert("Hello World!");
 let js = "amazing";
 // if (js === "amazing") alert("Javascript is FUN!");
 // js = "boring";
 // if (js === "amazing") alert("Javascript is FUN!");
-console.log(40 + 8 + 23 - 10);
+// console.log(40 + 8 + 23 - 10);
 console.log("Daniel");
 console.log(23);
 
 let firstName = "Daniel";
 console.log(firstName);
+
+// Variable name convention
+let daniel_mevs = "DM";
+let $function = 27;
+
+let person = "daniel";
+let PI = 3.1415;
+
+let myFirstJob = "Coder";
+let myCurrentJob = "Engineer";
+
+console.log(myFirstJob);
+
+*/
