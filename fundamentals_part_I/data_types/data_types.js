643cbd925224b170071f0e662f379f8e06829aab
DanielMevs <dannymevs@hotmail.com>
2024-04-12 21:12:04 -0400

    completed const, let, var and operators sections

--- fundamentals_part_I/data_types/data_types.js
+++ fundamentals_part_I/data_types/data_types.js
@@ -1,4 +1,4 @@
-let javascriptIsFun = true;
+/*let javascriptIsFun = true;
 console.log(javascriptIsFun);
 // console.log(typeof true);
 console.log(typeof javascriptIsFun);
@@ -16,3 +16,19 @@ year = 1991;
 console.log(typeof year);
 
 console.log(typeof null);
+*/
+
+let age = 30;
+age = 31;
+
+const birthYear = 1991;
+// const variables are immutable
+// birthYear = 1990;
+
+// const variables need to have an initializer
+// const job;
+
+// no no's
+// lastName = "Mevs";
+// console.log(lastName)
+// var variable1 = 'example';
---
+++ fundamentals_part_I/operators/operators.js
@@ -0,0 +1,30 @@
+// Math operators
+const now = 2037;
+
+// const ageDaniel = 2037 - 1991;
+// const ageSarah = 2037 - 2018;
+const ageJohn = now - 1991;
+const ageSarah = now - 2018;
+console.log(ageJohn, ageSarah);
+console.log(ageJohn * 2, ageSarah / 2);
+console.log(ageJohn * 2, ageJohn / 10, 2 ** 3);
+
+const firstName = "John";
+const lastName = "Doe";
+console.log(firstName + " " + lastName);
+
+// Assignment operators
+let x = 10 + 5; // 15
+x += 10; // x = x + 10
+x *= 4; // x = x * 4
+x++; // x = x + 1
+x--; // x = x - 1
+x--; // x = x - 1
+console.log(x);
+
+// Comparison operators
+console.log(ageJohn > ageSarah); // >, <, >=, <=
+console.log(ageSarah >= 18);
+
+const isFullAge = ageSarah >= 18;
+console.log(now - 1991 > now - 2018);
