4c52f9066c6925446a18d1bcb3a291caa0820282
DanielMevs <dannymevs@hotmail.com>
2024-04-18 22:18:22 -0400

    completed boolean operator and boolean logic sections

---
+++ fundamentals_part_I/boolean_logic/boolean.js
@@ -0,0 +1,23 @@
+const hasDriversLicense = true; // A
+const hasGoodVision = true; // B
+
+console.log(hasDriversLicense && hasGoodVision);
+console.log(hasDriversLicense || hasGoodVision);
+console.log(!hasDriversLicense);
+
+const shouldDrive = hasDriversLicense && hasGoodVision;
+
+// if (shouldDrive) {
+//   console.log("Sarah is able to drive!");
+// } else {
+//   console.log("Someone else should drive...");
+// }
+
+const isTired = false; // C
+console.log(hasDriversLicense && hasGoodVision && isTired);
+
+if (shouldDrive && !isTired) {
+  console.log("Sarah is able to drive!");
+} else {
+  console.log("Someone else should drive...");
+}
