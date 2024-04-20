c66d6d432e23cab4ef0634191e5b304176974123
DanielMevs <dannymevs@hotmail.com>
2024-04-20 12:33:34 -0400

    completed switch statement section

---
+++ fundamentals_part_I/switch_statement/switch.js
@@ -0,0 +1,24 @@
+const day = "monday";
+
+switch (day) {
+  case "monday": // day == 'monday'
+    console.log("Plan course structure");
+    console.log("Go to coding meetup");
+    break;
+  case "tuesday":
+    console.log("Prepare theory videos");
+    break;
+  case "wednesday":
+  case "thursday":
+    console.log("Write code examples");
+    break;
+  case "friday":
+    console.log("Record videos");
+    break;
+  case "saturday":
+  case "sunday":
+    console.log("Enjoy the weekend");
+    break;
+  default:
+    console.log("Not a valid day!");
+}
