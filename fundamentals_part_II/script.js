aba483fcc6a330fe93d61cb5a624cb2c7dc8435a
DanielMevs <dannymevs@hotmail.com>
2024-04-24 20:01:15 -0400

    completed function declaration, function expression and arrow functions sections

--- fundamentals_part_II/script.js
+++ fundamentals_part_II/script.js
@@ -10,7 +10,7 @@ if (hasDriversLicense) console.log("I can drive :D"); */
 // const interface = 'Audio';
 // const private = 534;
 // const if = 23;
-
+/*
 function logger() {
   console.log("My name is John");
 }
@@ -31,3 +31,45 @@ console.log(appleJuice);
 
 const appleOrangeJuice = fruitProcessor(2, 4);
 console.log(appleOrangeJuice);
+
+// Function declaration
+// const age1 = calcAge1(1991);
+
+function calcAge1(birthYear) {
+  return 2037 - birthYear;
+}
+
+const age1 = calcAge1(1991);
+
+console.log(age1);
+
+// Function expression
+const calcAge2 = function (birthYear) {
+  return 2037 - birthYear;
+};
+
+const age2 = calcAge2(1991);
+console.log(age1, age2);
+
+*/
+
+// Function expression
+const calcAge2 = function (birthYear) {
+  return 2037 - birthYear;
+};
+
+// Arrow function
+const calcAge3 = (birthYear) => 2037 - birthYear;
+
+const age3 = calcAge3(1991);
+console.log(age3);
+
+const yearsUntilRetirement = (birthYear, firstName) => {
+  const age = 2037 - birthYear;
+  const retirement = 65 - age;
+  // return retirement
+  return `${firstName} retires in ${retirement} years`;
+};
+
+console.log(yearsUntilRetirement(1991, "John"));
+console.log(yearsUntilRetirement(1980, "Bob"));
