6064eeea1f1934194dfcc620223cfdb5a81258b3
DanielMevs <dannymevs@hotmail.com>
2024-04-25 20:02:13 -0400

    completed calling functions from other functions section

--- fundamentals_part_II/script.js
+++ fundamentals_part_II/script.js
@@ -52,7 +52,7 @@ const age2 = calcAge2(1991);
 console.log(age1, age2);
 
 */
-
+/*
 // Function expression
 const calcAge2 = function (birthYear) {
   return 2037 - birthYear;
@@ -73,3 +73,36 @@ const yearsUntilRetirement = (birthYear, firstName) => {
 
 console.log(yearsUntilRetirement(1991, "John"));
 console.log(yearsUntilRetirement(1980, "Bob"));
+*/
+
+function cutFruitPieces(fruit) {
+  return fruit * 4;
+}
+function fruitProcessor(apples, oranges) {
+  const applePieces = cutFruitPieces(apples);
+  const orangePieces = cutFruitPieces(oranges);
+
+  // console.log(apples, oranges);
+  const juice = `Juice with ${applePieces} pieces of apple and ${orangePieces} pieces of orange.`;
+  return juice;
+}
+
+console.log(fruitProcessor(2, 3));
+
+const calcAge = function (birthYear) {
+  return 2037 - birthYear;
+};
+
+const yearsUntilRetirement = function (birthYear, firstName) {
+  const age = calcAge(birthYear);
+  const retirement = 65 - age;
+  if (retirement > 0) {
+    return retirement;
+  } else {
+    return -1;
+  }
+
+  // return `${firstName} retires in ${retirement}
+  // years`;
+};
+console.log(yearsUntilRetirement(1991, "Mike"));
