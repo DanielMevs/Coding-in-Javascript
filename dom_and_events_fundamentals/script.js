31c3ae914afb2baeb8a68e6e83e070434fba3c1a
DanielMevs <dannymevs@hotmail.com>
2024-05-06 20:02:07 -0400

    completed game logic and manipulating css section

--- dom_and_events_fundamentals/script.js
+++ dom_and_events_fundamentals/script.js
@@ -1,5 +1,5 @@
 'use strict';
-
+/*
 console.log(document.querySelector('.message').textContent);
 
 document.querySelector('.message').textContent = 'Correct Number!';
@@ -9,3 +9,47 @@ document.querySelector('.score').textContent = 20;
 
 document.querySelector('.guess').value = 23;
 console.log(document.querySelector('.guess').value);
+
+*/
+
+const secretNumber = Math.trunc(Math.random() * 20) + 1;
+let score = 20;
+document.querySelector('.number').textContent = secretNumber;
+
+document.querySelector('.check').addEventListener('click', function () {
+  const guess = Number(document.querySelector('.guess').value);
+  console.log(guess, typeof guess);
+  // When there is no input
+  if (!guess) {
+    document.querySelector('.message').textContent = 'No number!';
+  }
+  //When player wins
+  else if (guess == secretNumber) {
+    document.querySelector('.message').textContent = 'Correct Number!';
+    document.querySelector('body').style.backgroundColor = '#60b347';
+    document.querySelector('.number').style.width = '30rem';
+  }
+  // When guess is too high
+  else if (guess > secretNumber) {
+    if (score > 1) {
+      document.querySelector('.message').textContent = 'Too high!';
+      score--;
+    } else {
+      document.querySelector('.message').textContent = 'You lost the game! 🤯';
+    }
+  }
+  // When guess is too low
+  else if (guess < secretNumber) {
+    if (score > 1) {
+      document.querySelector('.message').textContent = 'Too low!';
+      score--;
+    } else {
+      document.querySelector('.message').textContent = 'You lost the game! 🤯';
+    }
+  }
+  document.querySelector('.score').textContent = score;
+});
+// document.querySelector('.check').addEventListener('click', function () {
+//   console.log(document.querySelector('.guess').value);
+//   document.querySelector('.message').textContent = 'Correct Number!';
+// });
