6593d856b49bba0a86cdb63ac1a18092c41dd487
DanielMevs <dannymevs@hotmail.com>
2024-05-07 20:03:15 -0400

    completed implement again button challenge

--- dom_and_events_fundamentals/script.js
+++ dom_and_events_fundamentals/script.js
@@ -12,9 +12,8 @@ console.log(document.querySelector('.guess').value);
 
 */
 
-const secretNumber = Math.trunc(Math.random() * 20) + 1;
+let secretNumber = Math.trunc(Math.random() * 20) + 1;
 let score = 20;
-document.querySelector('.number').textContent = secretNumber;
 
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
@@ -25,7 +24,8 @@ document.querySelector('.check').addEventListener('click', function () {
   }
   //When player wins
   else if (guess == secretNumber) {
-    document.querySelector('.message').textContent = 'Correct Number!';
+    document.querySelector('.message').textContent = '🎉 Correct Number!';
+    document.querySelector('.number').textContent = secretNumber;
     document.querySelector('body').style.backgroundColor = '#60b347';
     document.querySelector('.number').style.width = '30rem';
   }
@@ -53,3 +53,47 @@ document.querySelector('.check').addEventListener('click', function () {
 //   console.log(document.querySelector('.guess').value);
 //   document.querySelector('.message').textContent = 'Correct Number!';
 // });
+
+/// Coding Challenge 1:
+/*
+
+Implement the Again! functionality
+
+1. Select the element with the '.again' class
+and attach a click event handler
+
+2. In the handler function, restore initial
+value of the score and number variables
+
+3. Restore the initial conditions of the
+message, number, score and guess input 
+field.
+
+4. Also, restore the original background color
+(#222) and number width (15rem)
+
+*/
+document.querySelector('.again').addEventListener('click', function () {
+  // Resetting the number
+  let number = document.querySelector('.number');
+  number.textContent = '?';
+  number.style.width = '15rem';
+
+  // Resetting the background
+  let body = document.querySelector('body');
+  body.style.backgroundColor = '#222';
+
+  // Resetting the message
+  let message = document.querySelector('.message');
+  message.textContent = 'Start guessing...';
+
+  // Resetting the score and secret number
+  score = 20;
+  let scoreField = document.querySelector('.score');
+  scoreField.textContent = score;
+  secretNumber = Math.trunc(Math.random() * 20) + 1;
+
+  // Resetting input guess box
+  let guess = document.querySelector('input.guess');
+  guess.value = '';
+});
