36899ba00a2eb0498717cafb4a3e1174e2c40f87
DanielMevs <dannymevs@hotmail.com>
2024-06-23 17:05:33 -0400

    completed "passing arguments: value vs reference" section

--- a_closer_look_at_functions/script.js
+++ a_closer_look_at_functions/script.js
@@ -19,9 +19,36 @@ const createBooking = function (
   bookings.push(booking);
 };
 
-createBooking('LH123');
-createBooking('LH123', 2, 800);
-createBooking('LH123', 2);
-createBooking('LH123', 5);
+// createBooking('LH123');
+// createBooking('LH123', 2, 800);
+// createBooking('LH123', 2);
+// createBooking('LH123', 5);
 
-createBooking('LH123', undefined, 1000);
+// createBooking('LH123', undefined, 1000);
+
+const flight = 'LH234';
+const jonas = {
+  name: 'Jonas Schedtmann',
+  passport: 237208483,
+};
+
+const checkIn = function (flightNum, passenger) {
+  flightNum = 'LH999';
+  passenger.name = 'Mr.' + passenger.name;
+  if (passenger.passport == 237208483) {
+    alert('Check in');
+  } else {
+    alert('Wrong passport!');
+  }
+};
+
+// checkIn(flight, jonas);
+// console.log(flight);
+// console.log(jonas);
+
+const newPassport = function (person) {
+  person.passport = Math.trunc(Math.random() * 10000000);
+};
+
+newPassport(jonas);
+checkIn(flight, jonas);
