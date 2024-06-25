b694d8609d4ee81969e41fea119ad7411d6a505a
DanielMevs <dannymevs@hotmail.com>
2024-06-24 20:00:36 -0400

    completed first-class and higher-order functions and functions accepting callback functions sections

--- a_closer_look_at_functions/script.js
+++ a_closer_look_at_functions/script.js
@@ -46,9 +46,38 @@ const checkIn = function (flightNum, passenger) {
 // console.log(flight);
 // console.log(jonas);
 
-const newPassport = function (person) {
-  person.passport = Math.trunc(Math.random() * 10000000);
+// const newPassport = function (person) {
+//   person.passport = Math.trunc(Math.random() * 10000000);
+// };
+
+// newPassport(jonas);
+// checkIn(flight, jonas);
+
+const oneWord = function (str) {
+  return str.replace(/ /g, '').toLowerCase();
 };
 
-newPassport(jonas);
-checkIn(flight, jonas);
+const upperFirstWord = function (str) {
+  const [first, ...others] = str.split(' ');
+  return [first.toUpperCase(), ...others].join(' ');
+};
+
+// Higher-order function
+const transformer = function (str, fn) {
+  console.log(`Original string: ${str}`);
+  console.log(`Transformed string: ${fn(str)}`);
+  console.log(`Tranformed by : ${fn.name}`);
+  // return fn(str);
+};
+
+transformer('Javascript is the best!', upperFirstWord);
+transformer('Javascript is the best!', oneWord);
+
+// JS uses callbacks all the time
+const high5 = function () {
+  console.log('👋');
+};
+
+document.body.addEventListener('click', high5);
+
+['Jonas', 'Martha', 'Adam'].forEach(high5);
