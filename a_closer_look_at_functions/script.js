a4817659886b510474059e6f0e377facfc4cbdf3
DanielMevs <dannymevs@hotmail.com>
2024-07-01 20:13:35 -0400

    covered more closure examples

--- a_closer_look_at_functions/script.js
+++ a_closer_look_at_functions/script.js
@@ -301,19 +301,68 @@ runOnce();
 console.log(notPrivate);
 */
 
-const secureBooking = function () {
-  let passengerCount = 0;
+// const secureBooking = function () {
+//   let passengerCount = 0;
 
-  return function () {
-    passengerCount++;
-    console.log(`${passengerCount} passengers`);
+//   return function () {
+//     passengerCount++;
+//     console.log(`${passengerCount} passengers`);
+//   };
+// };
+
+// const booker = secureBooking();
+
+// booker();
+// booker();
+// booker();
+
+// console.dir(booker);
+
+// Example 1
+let f;
+
+const g = function () {
+  const a = 23;
+  f = function () {
+    console.log(a * 2);
   };
 };
 
-const booker = secureBooking();
+const h = function () {
+  const b = 777;
+  f = function () {
+    console.log(b * 2);
+  };
+};
 
-booker();
-booker();
-booker();
+g();
+f();
 
-console.dir(booker);
+console.dir(f);
+
+// Re-assigning f function
+h();
+f();
+
+console.dir(f);
+console.log(f);
+
+// Example 2
+const boardPassengers = function (n, wait) {
+  const perGroup = n / 3;
+
+  setTimeout(function () {
+    console.log(`We are now boarding all ${n} passengers`);
+    console.log(`There are 3 groups, each with ${perGroup} passengers`);
+  }, wait * 1000);
+
+  console.log(`Will start boarding in ${wait} seconds`);
+};
+
+// setTimeout(function () {
+//   console.log('TIMER');
+// }, 1000);
+
+// Closure has priority over scope-chain
+const perGroup = 1000;
+boardPassengers(180, 3);
