54ff92141a72087a20268a6421717accbe0ae36b
DanielMevs <dannymevs@hotmail.com>
2024-09-29 12:23:21 -0400

    implemented move marker on click feature

--- mapty/script.js
+++ mapty/script.js
@@ -4,6 +4,7 @@
 class Workout {
     date = new Date();
     id = (Date.now() + '').slice(-10);
+    clicks = 0;
 
     constructor(coords, distance, duration){
         this.coords = coords;  // [lat, lng]
@@ -18,6 +19,10 @@ class Workout {
         const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
 
         this.description = `${this.type[0].toUpperCase()}${this.type.slice(1)} on ${months[this.date.getMonth()]} ${this.date.getDate()}`;
+    }
+
+    click(){
+        this.clicks++;
     }
 
 }
@@ -75,6 +80,7 @@ const inputElevation = document.querySelector('.form__input--elevation');
 
 class App {
     #map;
+    #mapZoomLevel = 13;
     #mapEvent;
     #workouts = [];
 
@@ -82,8 +88,8 @@ class App {
 
         this._getPosition();
         form.addEventListener('submit', this._newWorkout.bind(this));
-        
         inputType.addEventListener('change', this._toggleElevationField);
+        containerWorkouts.addEventListener('click', this._moveToPopup.bind(this));
     }
 
     _getPosition(){
@@ -258,6 +264,25 @@ class App {
         }
 
         form.insertAdjacentHTML('afterend', html);
+    }
+    _moveToPopup(e){
+        const workoutEl = e.target.closest('.workout');
+        console.log(workoutEl);
+
+        if(!workoutEl) return;
+
+        const workout = this.#workouts.find(work => work.id === workoutEl.dataset.id);
+        console.log(workout);
+
+        this.#map.setView(workout.coords, this.#mapZoomLevel, {
+            animate: true,
+            pan: {
+                duration: 1,
+            }
+        });
+
+        // Using the public interface
+        workout.click();
     }
 }
 
