d2ca2233efabf1c4e44df600cf41695eb1663137
DanielMevs <dannymevs@hotmail.com>
2024-10-08 20:09:19 -0400

    covered importing and exporting ES6 modules

---
+++ modern_javascript_modules_tooling/script.js
@@ -0,0 +1,49 @@
+// Importing module
+// import { 
+//     addToCart,
+//     totalPrice as price,
+//     tq 
+// } from "./shoppingCart";
+
+
+console.log('Importing module');
+
+// addToCart('bread', 5);
+// console.log(price, tq);
+
+// import * as ShoppingCart from './shoppingCart.js';
+// ShoppingCart.addToCart('bread', 5);
+// console.log(ShoppingCart.totalPrice);
+
+// Typically, one should avoid mxing named and default exports.
+
+import add, { cart } from './shoppingCart.js';
+
+add('pizza', 2);
+add('bread', 5);
+add('apples', 4);
+
+console.log(cart);
+
+// console.log('Start fetching');
+// const res = await fetch('https://jsonplaceholder.typicode.com/posts');
+// const data = await res.json()
+// console.log(data);
+// console.log('Something');
+
+const getLastPost = async function() {
+    const res = await fetch('https://jsonplaceholder.typicode.com/posts');
+    const data = await res.json();
+    console.log(data);
+
+    return {title: data.at(-1).title, text: data.at(-1).body };
+}
+
+const lastPost = getLastPost();
+console.log(lastPost);
+
+// Not very clean
+// lastPost.then(last => console.log(last));
+
+const lastPost2 = await getLastPost();
+console.log(lastPost2);
