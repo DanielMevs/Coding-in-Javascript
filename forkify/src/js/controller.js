5f4872652e86d182f4eb374782a0a0ad0157742c
DanielMevs <dannymevs@hotmail.com>
2024-10-15 19:57:28 -0400

    Added files for helper functions and configuration details.

--- forkify/src/js/controller.js
+++ forkify/src/js/controller.js
@@ -8,14 +8,6 @@ import 'regenerator-runtime/runtime';
 
 const recipeContainer = document.querySelector('.recipe');
 
-const timeout = function (s) {
-  return new Promise(function (_, reject) {
-    setTimeout(function () {
-      reject(new Error(`Request took too long! Timeout after ${s} second`));
-    }, s * 1000);
-  });
-};
-
 // https://forkify-api.herokuapp.com/v2
 
 ///////////////////////////////////////
@@ -37,8 +29,8 @@ const controlRecipes = async function () {
   }
 };
 
-// controlRecipes();
+const init = function () {
+  recipeView.addHandlerRender(controlRecipes);
+};
 
-['hashchange', 'load'].forEach(ev =>
-  window.addEventListener(ev, controlRecipes)
-);
+init();
--- forkify/src/model.js
+++ forkify/src/model.js
@@ -1,4 +1,6 @@
 import { async } from 'regenerator-runtime';
+import { API_URL } from './js/config';
+import { getJSON } from './js/helpers';
 
 export const state = {
   recipe: {},
@@ -6,9 +8,7 @@ export const state = {
 
 export const loadRecipe = async function (id) {
   try {
-    const res = await fetch(
-      `https://forkify-api.herokuapp.com/api/v2/recipes/${id}`
-    );
+    const res = await fetch(`${API_URL}/${id}`);
     const data = await res.json();
 
     if (!res.ok) throw new Error(`${data.message} (${res.status})`);
@@ -26,7 +26,6 @@ export const loadRecipe = async function (id) {
     };
     console.log(state.recipe);
   } catch (err) {
-    alert(`${err} 💥💥💥💥`);
-    throw err;
+    console.error(`${err} 💥💥💥💥`);
   }
 };
